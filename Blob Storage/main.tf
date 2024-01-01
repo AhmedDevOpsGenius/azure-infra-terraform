1fda8e44817b5baf5bade8856b229ca281734792
Ahmed Hassan <64609713+AhmedDevOpsGenius@users.noreply.github.com>
2024-01-02 04:45:50 +0500

    Create main.tf

---
+++ Blob Storage/main.tf
@@ -0,0 +1,55 @@
+terraform {
+required_providers {
+azurerm={
+source="hashicorp/azurerm"
+version=">= 3.0.0"
+    }
+  }
+}
+
+provider"azurerm" {
+features {}
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Name of the storage account"
+  default     = "uniqueprefixaccstore"
+}
+
+variable "location" {
+  type        = string
+  description = "Location for the resources"
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "example" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_storage_container" "example" {
+  name                  = "content"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+
+resource "azurerm_storage_blob" "example" {
+  name                   = "my-awesome-content.zip"
+  storage_account_name   = azurerm_storage_account.example.name
+  storage_container_name = azurerm_storage_container.example.name
+  type                   = "Block"
+  source                 = "some-local-file.zip"
+}
